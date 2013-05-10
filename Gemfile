2afb90067c9fee580c281a453326fd88081afa05
Johan Sveholm <jsveholm@gmail.com>
2013-05-10 08:56:43 +0200

    added therubyracer

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sass-rails',   '~> 4.0.0.rc1'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'compass-rails'
+gem 'therubyracer', platforms: :ruby
 
 #rails 4 cut out gems
 #gem 'protected_attributes'
--- db/schema.rb
+++ db/schema.rb
@@ -9,38 +9,38 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419081050) do
+ActiveRecord::Schema.define(version: 20130419081050) do
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "sections", :force => true do |t|
+  create_table "sections", force: true do |t|
     t.text     "content"
     t.integer  "post_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "section_id"
   end
 
-  create_table "taggings", :force => true do |t|
+  create_table "taggings", force: true do |t|
     t.integer  "tag_id"
     t.integer  "section_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string   "name"
     t.string   "ancestry"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "tags", ["ancestry"], :name => "index_tags_on_ancestry"
+  add_index "tags", ["ancestry"], name: "index_tags_on_ancestry", using: :btree
 
 end
