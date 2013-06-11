082634bfd02374e500fa1cacd93175f19f2058f8
Johan Sveholm <jsveholm@gmail.com>
2013-06-11 11:23:10 +0900

    unicorn init update

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,32 +1,45 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          unicorn
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Manage unicorn server
+# Description:       Start, stop, restart unicorn server for a specific application.
+### END INIT INFO
 set -e
-# Example init script, this can be used with nginx, too,
-# since nginx and unicorn accept the same signals
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/habu/apps/blog/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-action="$1"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+AS_USER=habu
 set -u
 
-old_pid="$PID.oldbin"
-
-cd $APP_ROOT || exit 1
+OLD_PIN="$PID.oldbin"
 
 sig () {
   test -s "$PID" && kill -$1 `cat $PID`
 }
 
 oldsig () {
-  test -s $old_pid && kill -$1 `cat $old_pid`
+  test -s $OLD_PIN && kill -$1 `cat $OLD_PIN`
 }
 
-case $action in
+run () {
+  if [ "$(id -un)" = "$AS_USER" ]; then
+    eval $1
+  else
+    su -c "$1" - $AS_USER
+  fi
+}
+
+case "$1" in
 start)
   sig 0 && echo >&2 "Already running" && exit 0
-  su -c "$CMD" - habu
+  run "$CMD"
   ;;
 stop)
   sig QUIT && exit 0
@@ -39,27 +52,27 @@ force-stop)
 restart|reload)
   sig HUP && echo reloaded OK && exit 0
   echo >&2 "Couldn't reload, starting '$CMD' instead"
-  su -c "$CMD" - habu
+  run "$CMD"
   ;;
 upgrade)
   if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
   then
     n=$TIMEOUT
-    while test -s $old_pid && test $n -ge 0
+    while test -s $OLD_PIN && test $n -ge 0
     do
       printf '.' && sleep 1 && n=$(( $n - 1 ))
     done
     echo
 
-    if test $n -lt 0 && test -s $old_pid
+    if test $n -lt 0 && test -s $OLD_PIN
     then
-      echo >&2 "$old_pid still exists after $TIMEOUT seconds"
+      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
       exit 1
     fi
     exit 0
   fi
   echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-  su -c "$CMD" - habu
+  run "$CMD"
   ;;
 reopen-logs)
   sig USR1
@@ -69,3 +82,4 @@ reopen-logs)
   exit 1
   ;;
 esac
+
